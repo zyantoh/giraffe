53f9645766ed6b771c3b902a48e38768137e0c28
Joel Tio <joeltiojinhon@gmail.com>
2019-11-12 23:24:46 +0800

    Remove service account file

--- rpi/firestore/database.py
+++ rpi/firestore/database.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
-from google.cloud import firestore
+from firebase_admin import firestore
 from enum import Enum
 
 
@@ -8,7 +8,7 @@ def create_client() -> firestore.Client:
     cred = credentials.Certificate('service_account.json')
     firebase_admin.initialize_app(cred)
 
-    return firebase_admin.firestore.client()
+    return firestore.client()
 
 
 class TableStatus(Enum):
@@ -22,6 +22,6 @@ def update_table_status(
         canteen_name: str,
         table_id: str,
         new_status: TableStatus):
-    doc_ref = db.collection(u"canteen").document(
+    doc_ref = client.collection(u"canteen").document(
         canteen_name, "tables", table_id, "isTaken")
     doc_ref.set(new_status.value)
