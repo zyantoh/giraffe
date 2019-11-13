841ac34f933212b5e47a0d0f585a6003f1b8b0f5
Joel Tio <joeltiojinhon@gmail.com>
2019-11-14 01:17:40 +0800

    Add CanteenRepository

--- android/app/src/main/java/com/giraffe/canteen/data/CanteenRepository.kt
+++ android/app/src/main/java/com/giraffe/canteen/data/CanteenRepository.kt
@@ -2,24 +2,21 @@ package com.giraffe.canteen.data
 
 import com.giraffe.canteen.model.Canteen
 import com.giraffe.database.DatabaseService
+import com.giraffe.storage.StorageService
 
 class CanteenRepository(
-    private val databaseService: DatabaseService
+    private val databaseService: DatabaseService,
+    private val storageService: StorageService
 ) {
-    fun getCanteenDetails(
-        successCallback: (List<Canteen>) -> Unit,
-        failureCallback: (Exception) -> Unit,
-        canceledCallback: () -> Unit
-    ) {
-        databaseService.collection("canteens").get(
-            successCallback = {documentSnapshots ->
-                val canteenNames = documentSnapshots.map {
-                    Canteen(name = it.id, location = it.get("location") as String)
-                }
-                successCallback(canteenNames)
-            },
-            failureCallback = failureCallback,
-            canceledCallback = canceledCallback
-        )
+    suspend fun getCanteenDetails(): List<Canteen> {
+        val canteenDocuments = databaseService.collection("canteens").get()
+        return canteenDocuments.map {
+            val thumbnailUri = storageService.getURI(it.get("thumbnail") as String)
+            Canteen(
+                name = it.id,
+                location = it.get("location") as String,
+                thumbnailUri = thumbnailUri
+            )
+        }
     }
 }
