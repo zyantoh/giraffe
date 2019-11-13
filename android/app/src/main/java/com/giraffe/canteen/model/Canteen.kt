bc1c60180cf62c29d571db48fc25f3a5f24f8250
Joel Tio <joeltiojinhon@gmail.com>
2019-11-14 01:18:13 +0800

    Add thumbnail Uri to Canteen model

--- android/app/src/main/java/com/giraffe/canteen/model/Canteen.kt
+++ android/app/src/main/java/com/giraffe/canteen/model/Canteen.kt
@@ -1,5 +1,12 @@
 package com.giraffe.canteen.model
 
-data class Canteen(val name: String, val location: String) {
+import android.net.Uri
+
+
+data class Canteen(
+    val name: String,
+    val location: String,
+    val thumbnailUri: Uri
+) {
     var tables: Map<String, Table>? = null
 }
