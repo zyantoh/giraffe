773e044b7bba66b92c91ce5b687982f7d5b49f54
Zhi Wen Jeff Tan <s10186971@connect.np.edu.sg>
2020-01-15 12:21:09 +0800

    Bottom Navigation Bar

--- android/app/src/main/java/com/giraffe/MainActivity.kt
+++ android/app/src/main/java/com/giraffe/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.giraffe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    fun setUpNavigation() {
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            (nav_host_fragment!! as NavHostFragment).navController
+        )
     }
 }
