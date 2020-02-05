3f3f0981abe197ae711599fde858c724ded5100c
Joel Tio <joeltiojinhon@gmail.com>
2020-02-05 12:45:57 +0800

    Add stalls to canteen fragment

--- android/app/src/main/java/com/giraffe/canteen/ui/CanteenFragment.kt
+++ android/app/src/main/java/com/giraffe/canteen/ui/CanteenFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -56,6 +57,12 @@ class CanteenFragment : Fragment() {
         // Set the button on click listener
         view_map_button.setOnClickListener {
             findNavController().navigate(R.id.action_canteenFragment_to_canteenMapFragment, arguments)
+        }
+
+        if (canteen.stalls != null) {
+            val stallNames = canteen.stalls.map { it.name }
+            canteen_stall_list_recyclerView.adapter =
+                ArrayAdapter<String>(context!!, android.R.layout.simple_list_item_1, stallNames)
         }
     }
 }
