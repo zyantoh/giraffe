e290e36917ea17b413d3ef941889ca3d09afc470
Joel Tio <joeltiojinhon@gmail.com>
2020-02-05 17:24:06 +0800

    Add and fix code for firebase

--- rpi/firestore/database.py
+++ rpi/firestore/database.py
@@ -1,27 +1,37 @@
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import firestore, storage
 from enum import Enum
+
+
+FOOD_CLUB_ID = "RUVNqof5cTdIkX5krJYP"
+FOOD_CLUB_FILE = "RUVNqof5cTdIkX5krJYP.jpg"
+BUCKET_NAME = "giraffe-1.appspot.com"
 
 
 def create_client() -> firestore.Client:
     cred = credentials.Certificate('service_account.json')
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(cred, {
+        "storageBucket": "giraffe-1.appspot.com",
+    })
 
     return firestore.client()
 
 
-class TableStatus(Enum):
-    CONFIDENT_TAKEN = "CONFIDENT_TAKEN"
-    MAYBE_TAKEN = "MAYBE_TAKEN"
-    CONFIDENT_NOT_TAKEN = "CONFIDENT_NOT_TAKEN"
-
-
-def update_table_status(
+def update_table_occupancy(
         client: firestore.Client,
-        canteen_name: str,
-        table_id: str,
-        new_status: TableStatus):
-    doc_ref = client.collection(u"canteen").document(
-        canteen_name, "tables", table_id, "isTaken")
-    doc_ref.set(new_status.value)
+        occupied: int):
+    canteen_id = FOOD_CLUB_ID
+    doc_ref = client.collection(u"canteens").document(
+        canteen_id)
+    doc_ref.update({
+        "occupiedTables": occupied
+    })
+
+
+def upload_image(
+    filename: str,
+):
+    bucket = storage.bucket(BUCKET_NAME)
+    blob = bucket.blob(f"canteens/{FOOD_CLUB_FILE}")
+    blob.upload_from_filename(filename)
